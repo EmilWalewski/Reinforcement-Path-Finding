ededb2bddae945169f0cb64bc3a2a0d3dc0dcb0c
Your Name <you@example.com>
2023-02-19 19:26:53 +0100

    exit program if treasure is next to start point

--- path_finding.py
+++ path_finding.py
@@ -7,6 +7,7 @@ maze[2][2] = 1
 maze[3][3] = 1
 maze[4][3] = 1
 
+
 class Env:
     def __init__(self, maze):
         self.maze = maze
@@ -71,23 +72,18 @@ class State:
 
 
 class Agent:
-    def __init__(self, env):
+    def __init__(self, env, start_point, reward_point):
         self.env = env
+        self.start_point = start_point
+        self.reward_point = reward_point
 
     def move(self):
         done = False
         steps = ['up', 'down', 'right', 'left']
-        current_state = State((4, 2), None)
+        current_state = State(self.start_point, None)
         iterations = 0
         visited_nodes = {current_state.position: 0}
         while not done:
-            if current_state.position == (4, 1):
-                for state in current_state.next_states:
-                    print(f'{current_state.position} -> {state.position} : {state.reward} : {state.is_visited}')
-                print('------------------------------------')
-                print(f'Iterations = {iterations}')
-                print('Treasure found')
-                break
 
             iterations += 1
             current_state.is_visited = True
@@ -95,6 +91,14 @@ class Agent:
             if len(current_state.next_states) == 0:
                 for idx, step in enumerate(steps):
                     new_position, reward = self.env.reward(current_state.position, step)
+
+                    if new_position == self.reward_point:
+                        print(f'Go to {current_state.position} -> {new_position}')
+                        print(f'Iterations = {iterations}')
+                        print('Treasure found')
+                        done = True
+                        break
+
                     state = State(new_position, current_state)
                     try:
                         visited_nodes[new_position]
@@ -103,23 +107,26 @@ class Agent:
                     state.update_reward(visited_nodes[new_position])
                     current_state.add_next_state(state)
 
-            for idx, next_state in enumerate(current_state.next_states):
-                if next_state.reward >= current_state.reward and next_state.is_visited == False:
-                    print(f'Go to {current_state.position} -> {next_state.position}')
-                    current_state = next_state
-                    next_state_exists = True
-                    break
+            if not done:
+                for idx, next_state in enumerate(current_state.next_states):
+                    if next_state.reward >= current_state.reward and next_state.is_visited == False:
+                        print(f'Go to {current_state.position} -> {next_state.position}')
+                        current_state = next_state
+                        next_state_exists = True
+                        break
 
-            if next_state_exists:
-                continue
+                if next_state_exists:
+                    continue
 
-            if current_state.previous_state.is_visited:
-                print(f'Back {current_state.position} -> {current_state.previous_state.position}')
-                current_state.previous_state.update_location(current_state)
-                current_state = current_state.previous_state
+                if current_state.previous_state.is_visited:
+                    print(f'Back {current_state.position} -> {current_state.previous_state.position}')
+                    current_state.previous_state.update_location(current_state)
+                    current_state = current_state.previous_state
 
 
 env = Env(maze)
-agent = Agent(env)
+start_point = (4, 2)
+reward_point = (4, 0)
+agent = Agent(env, start_point, reward_point)
 print(env.maze)
 agent.move()
